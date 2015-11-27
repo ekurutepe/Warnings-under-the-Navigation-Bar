4a1d195039b628d99b0330afb85bef1f47c309a2
Engin Kurutepe <engin@kurutepe.com>
2015-11-27 16:17:41 +0100

    Initial Commit

---
+++ navigation bar warnings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  navigation bar warnings
+//
+//  Created by Engin Kurutepe on 27/11/15.
+//  Copyright © 2015 Engin Kurutepe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ navigation bar warnings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  navigation bar warnings
+//
+//  Created by Engin Kurutepe on 27/11/15.
+//  Copyright © 2015 Engin Kurutepe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
